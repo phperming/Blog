ff1d76f70b5dc7fdcaa35bd465f9499e069b1d32
phperming <875617373@qq.com>
2021-01-07 22:48:30 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter,r *http.Request)  {
+	w.Header().Set("Content-Type","text/html;charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w,"<h1>欢迎来到goBlog</h>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<p>此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a></p>")
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
 		fmt.Fprint(w,"你要找的页面不存在")
 	}
 
-}	
+}
 
 func main()  {
 	http.HandleFunc("/",handlerFunc)

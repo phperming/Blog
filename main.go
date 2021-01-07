25ac00fccc07b6edf793a6c7f7dbcd4d0857bd13
phperming <875617373@qq.com>
2021-01-07 23:47:30 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -3,23 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter,r *http.Request)  {
+func defaultHandler(w http.ResponseWriter,r *http.Request) {
 	w.Header().Set("Content-Type","text/html;charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w,"<h1>欢迎来到goBlog</h>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+		fmt.Fprint(w,"<h1>Hello ,欢迎来到 GOBlog</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w,"你要找的页面不存在")
+		fmt.Fprint(w,"<h1>您要访问的页面不存在:(</h1> <p> 如有疑惑请联系我们</p>")
 	}
+}
+
+func aboutHandler(w http.ResponseWriter,r *http.Request)  {
+	w.Header().Set("Content-Type","text/html;charset=utf-8")
+
+	fmt.Fprint(w,"此博客是用来记录变成笔记，如有反馈和建议请联系<a href=\"mailto:michel@163.com\">Michel@163.com</a>")
 
 }
 
 func main()  {
-	http.HandleFunc("/",handlerFunc)
-	http.ListenAndServe(":8088",nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/",defaultHandler)
+	router.HandleFunc("/about",aboutHandler)
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path,"/",3)[2]
+		fmt.Fprint(w,"文章 ID:" + id)
+	})
+
+	http.ListenAndServe(":8088",router)
 }

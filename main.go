5c3c5cc71130ec0045077fe0a1341a91b066f209
phperming <875617373@qq.com>
2021-01-09 20:42:32 +0800

    中间件

--- main.go
+++ main.go
@@ -4,28 +4,24 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
 	fmt.Fprint(w,"<h1>Hello,欢迎来到GoBlog!</h1>")
 }
 
 func notFoundHandler(w http.ResponseWriter,r *http.Request) {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w,"<h1>您要访问的页面不存在:(</h1> <p> 如有疑惑请联系我们</p>")
 
 }
 
 func aboutHandler(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
-
 	fmt.Fprint(w,"此博客是用来记录变成笔记，如有反馈和建议请联系<a href=\"mailto:michel@163.com\">Michel@163.com</a>")
 }
 
 func articlesShowHandler(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Fprint(w,"文章 ID:" + id)
@@ -38,6 +34,28 @@ func articlesIndexHandler(w http.ResponseWriter,r *http.Request)  {
 func articlesStoreHandler(w http.ResponseWriter,r *http.Request) {
 	fmt.Fprint(w,"创建新的文章")
 }
+
+func forceHTMLMiddleware(next http.Handler)http.Handler  {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//1.设置标头
+		w.Header().Set("Content-Type","text/html;charset=utf-8")
+		//2.继续处理请求
+		next.ServeHTTP(w,r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//除首页以外，移除所有请求路径后面的/
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path,"/")
+		}
+
+		//将请求传递下去
+		next.ServeHTTP(w,r)
+	})
+}
+
 
 func main()  {
 	router := mux.NewRouter()
@@ -52,11 +70,14 @@ func main()  {
 	//自定义404页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
+	//中间件 强制内容类型为HTML
+	router.Use(forceHTMLMiddleware)
+
 	//通过命名路由获取URL示例
 	homeURL ,_:= router.Get("home").URL()
 	fmt.Println(homeURL)
 	articleURL ,_:= router.Get("articles.show").URL()
 	fmt.Println(articleURL)
 
-	http.ListenAndServe(":8088",router)
+	http.ListenAndServe(":8088",removeTrailingSlash(router))
 }

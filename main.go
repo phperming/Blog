264fe82efce82a8e26748980d83ff371cc77ff84
phperming <875617373@qq.com>
2021-01-09 23:23:34 +0800

    创建数据库表

--- main.go
+++ main.go
@@ -50,6 +50,18 @@ func checkError(err error)  {
 	}
 }
 
+func createTables()  {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+		id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+		title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+		body longtext COLLATE utf8mb4_unicode_ci
+	);`
+
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+
+}
+
 type ArticlesFormData struct {
 	Title string
 	Body string
@@ -164,6 +176,8 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func main()  {
+	initDB()
+	createTables()
 	router.HandleFunc("/",homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about",aboutHandler).Methods("GET").Name("about")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")

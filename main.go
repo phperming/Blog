48042b2e69f03ac57ef0174a193b6dd2dcac6d03
phperming <875617373@qq.com>
2021-01-09 23:00:43 +0800

    连接数据库

--- main.go
+++ main.go
@@ -2,16 +2,53 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
+	"database/sql"
 )
 
 var router = mux.NewRouter()
+var db  *sql.DB
+
+func initDB()  {
+	var err error
+	config := mysql.Config{
+		User : "root",
+		Passwd : "root",
+		Addr: "127.0.0.1:3306",
+		Net: "tcp",
+		DBName: "goblog",
+		AllowNativePasswords: true,
+	}
+
+	//准备数据库连接池
+	db,err := sql.Open("mysql",config.FormatDSN())
+	checkError(err)
+	//设置最大连接数
+	db.SetMaxOpenConns(25)
+	//设置最大空闲数
+	db.SetMaxIdleConns(25)
+	//设置每个连接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	//尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
+}
+
+func checkError(err error)  {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type ArticlesFormData struct {
 	Title string
